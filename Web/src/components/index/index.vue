fce62d500187c753f4ca1feb106e45d16aba0479
zsn <shengnan586@163.com>
2019-08-13 20:27:06 +0800

    轮播图

---
+++ Web/src/components/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div>
+      <carousel></carousel> 
+      <div class="search">
+        
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import Carousel from "./Carousel.vue"
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  components:{
+    "carousel":Carousel
+  }
+}
+</script>
+<style scoped>
+  
+</style>
--- Web/src/router.js
+++ Web/src/router.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloContainer from "./components/HelloWorld.vue"
+
+import index from "./components/index/index.vue"
 Vue.use(Router)
 export default new Router({
   routes: [
+    {path:'/index',component:index},
     {path:'/',component:HelloContainer},
   ]
 })
