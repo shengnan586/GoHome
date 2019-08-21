137b8ea35abe7e7c8242b3ec4bd4aa3317e412c6
zsn <shengnan586@163.com>
2019-08-22 01:30:01 +0800

    detailtop

--- Web/src/router.js
+++ Web/src/router.js
@@ -15,11 +15,13 @@ import Reg_go from "./components/Reg_go.vue"
 import Publish from "./components/publish/Publish.vue"
 import Facility from "./components/publish/Facility.vue"
 import Describe from "./components/publish/Describe.vue"
+import detail from './components/productlist/detail.vue'
 
 Vue.use(Router)
 export default new Router({
   routes: [
     {path:'/index',component:index},
+    {path:'/detail',component:detail},
     {path:'/',component:HelloContainer},
     {path:'/order',component:Order},
     {path:'/productList',component:productList},
