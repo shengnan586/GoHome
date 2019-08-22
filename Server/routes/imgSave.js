3245b96afe4a3286593f8c7a681b1c1ddbe5640d
moonpie-nn <15871356991@163.com>
2019-08-22 16:12:19 +0800

    第四部照片接口更改

--- Server/routes/imgSave.js
+++ Server/routes/imgSave.js
@@ -1,9 +1,28 @@
-const express=require("express");
-const router=express.Router();
-const query=require("../query");
+const express = require("express");
+const router = express.Router();
+const query = require("../query");
 
-router.post("/",(req,res)=>{
-
+router.post("/", (req, res) => {
+    var obj = req.body;
+    var sql = "delete from home_business_house_img where hid = ?";
+    query(sql, [obj.hid])
+        .then(result => {
+            var str = "";
+            var arr = [];
+            for (var i = 0;i < obj.url.length;i++) {
+                str += "(?,?,'md'),";
+                arr.push(parseInt(hid));
+                arr.push(parseInt(obj.url[i]));
+            }
+            str = str.substr(0, str.length - 1);
+            sql = "INSERT into  home_business_house_img (hId,ImgUrl,ImgSize)  VALUES ${str} ";
+            return query(sql,arr);
+        })
+        .then(result=>{
+            if(result.affectedRows > 0){
+                res.send({code:1});
+            }
+        })
 })
 
 module.exports = router;
