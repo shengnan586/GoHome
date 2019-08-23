2947ed83e5d1c55ba14a0c893f1558f70ece1295
moonpie-nn <15871356991@163.com>
2019-08-23 21:17:15 +0800

    冲突更改

--- Web/src/components/publish/Publish.vue
+++ Web/src/components/publish/Publish.vue
@@ -102,9 +102,8 @@ export default {
           default: "-145px -2359px",
           active: "-91px -2400px"
         }
-<<<<<<< HEAD
-      ]
-    };
+      ]  
+    }   
   },
   watch: {
     step() {
@@ -115,31 +114,6 @@ export default {
       }
     }
   },
-  components: { Facility, Info, Describe, Footer: footer,
-    Header: header },
-  created() {
-    if (!this.$route.params.hid) this.hid = 0;
-    //如果没传hid hid则为0
-    else this.hid = this.$route.params.hid; //页面刚加载就读取传过来的hid
-    //如果是新页面 则step=1 只有第一个字绿色，如果是继续填写，step的值由子组件查到的数据决定
-    for (var i = 1; i < this.step; i++) {
-      this.word[i].isActive = true;
-      this.word[i].position = this.word[i].active;
-    }
-  },
-  methods: {
-    //只有当字体为绿色时才能点击切换组件
-    tab(e) {
-      if (e.target.nodeName == "P" || e.target.nodeName == "SPAN") {
-        var num = parseInt(e.target.dataset.num);
-        if (this.word[num].isActive) {
-          //跳转到点击的页面，数组下标是012 所以页面数要加1
-          this.stepToChild = num + 1;
-        } else {
-          return;
-=======
-      }
-    },
     components:{Facility,Info,Describe,Photos,Price},
     created() {
       if(!this.$route.params.hid) this.hid = 23;//如果没传hid hid则为0
@@ -164,24 +138,6 @@ export default {
           }
         }
       },
-      stepToParent(step){
-        if(this.step <= step){
-          this.step = step+1;//控制字体颜色 让下一个组件的颜色变亮 +1
-        }
-        //this.stepToParent = 0;
-        this.stepToChild = step+1;//控制跳下一个组件
-      },
-      sendHid(hid){
-        this.hid = hid;
-      },
-      sort(step){//该函数控制如果当前页面是从房源发布列表id中进入 显示当时保存的页面
-        if(this.step < step){
-          this.step = step;
-          this.stepToChild = step;
->>>>>>> 6d2a63fc22b1d95350dccdc7b0cf85f9966ff751
-        }
-      }
-    },
     stepToParent(step) {
       console.log("fanhui" + step);
       if (this.step <= step) {
@@ -222,14 +178,11 @@ export default {
   font: "Microsoft YaHei";
   font-weight: bold;
 }
-<<<<<<< HEAD
-.after {
-  margin-left: 125px;
-=======
+
 .after{
   /* margin-left:125px; */
   margin-left:11%;
->>>>>>> 6d2a63fc22b1d95350dccdc7b0cf85f9966ff751
+
 }
 p {
   margin: 0;
@@ -260,28 +213,16 @@ p.word {
   height: 2px;
   background: #eeeeee;
   position: absolute;
-<<<<<<< HEAD
-  top: 40px;
-  left: 338px;
-=======
+
   top:40px;
   /* left:338px; */
   left:23.5%;
->>>>>>> 6d2a63fc22b1d95350dccdc7b0cf85f9966ff751
+
 }
 span.span_active {
   background-color: #30c3a6;
 }
-<<<<<<< HEAD
-span.line2 {
-  left: 519px;
-}
-span.line3 {
-  left: 700px;
-}
-span.line4 {
-  left: 880px;
-=======
+
 span.line2{
   /* left:519px; */
   left:36%;
@@ -293,7 +234,7 @@ span.line3{
 span.line4{
   /* left:880px; */
   left:61.3%
->>>>>>> 6d2a63fc22b1d95350dccdc7b0cf85f9966ff751
+
 }
 .cursor {
   cursor: pointer;
