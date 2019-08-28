9448bc517c942d7a46c2d2a6817cc6694ae448b5
zsn <shengnan586@163.com>
2019-08-29 00:17:35 +0800

    部署

--- Web/src/main.js
+++ Web/src/main.js
@@ -11,7 +11,7 @@ Vue.config.productionTip = false
 
 //引入axios模块
 import Axios from 'axios';
-Axios.defaults.baseURL="http://127.0.0.1:3003/"
+Axios.defaults.baseURL="http://web1904bugapp.applinzi.com/"
 Axios.defaults.withCredentials=true;
 Vue.prototype.axios=Axios;
 Vue.prototype.$echarts = echarts;
